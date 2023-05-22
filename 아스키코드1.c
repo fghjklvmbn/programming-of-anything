a0fc7bc12250033c9c99a13bcfccd7d5ebe259b5
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-05-23 04:50:03 +0900

    c언어 연습

---
+++ 아스키코드1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    char x = 65;
+    char y = 'A';
+
+    printf("x = %c\n", x);
+    printf("y = %c\n", y);
+
+    //삐 소리 내기
+    char beep = 7;
+    printf("%c", beep);
+
+    return 0;
+}
---
+++ 제어문자.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int id, pass;
+
+    printf("아이디와 패스워드를 입력해주세요\n");
+    printf("id : ____\b\b\b\b");
+    scanf("%d", &id);
+    printf("\npassword : ____\b\b\b\b");
+    scanf("%d", &pass);
+
+    printf("입력하신 아이디는 %d 이며, 비밀번호는 %d 입니다.", id, pass);
+    return 0;
+}
