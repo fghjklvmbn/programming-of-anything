36d0c4f3f26b3c994688adf8ade5b353ed25a5df
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-05-24 14:36:10 +0900

    수정/연습파일 push

--- 자바연습폴더/연습.java
+++ 자바연습폴더/연습.java
@@ -1,17 +1,15 @@
-import java.io.*;;
-
-class park {
-    int field1;
-    String field2;
-    public void method() {
-        System.out.println("hello! world!");
+import java.time.LocalDate;
+public class 연습 {
+    public static void main(String[] args) {
+        System.out.print(7);
+        System.out.println(3);
+        System.out.println(3.14);
+        System.out.println("자바");
+        System.out.println("문자열 끼리"+" 연결이 가능하답니다.");
+        System.out.println("숫자" + 3 + "과 문자열의 연결도 가능하답니다.");
+        LocalDate today = LocalDate.now();
+        System.out.println("올해는" + today.getYear() + "년입니다.");
+        LocalDate back = today.withYear(1923);
+        System.out.println("올해는" + back.getYear() + "년입니다.");
     }
-
 }
-public class prog {
-	public static void main(String[] args) {
-		park mypark = new park();
-		mypark.method();
-	}
-}
-
---
+++ 자바연습폴더/연습1.java
@@ -0,0 +1,12 @@
+public class 연습1 {
+    public static void main(String[] args) {
+        int num;
+        num = 10;
+        System.out.println(num);
+
+        double a, b;
+        a = 3.141592;
+        b = 6.283185;
+        System.out.println(a+b);
+    }
+}
