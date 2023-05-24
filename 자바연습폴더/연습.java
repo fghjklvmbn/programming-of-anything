b5173af4852e3a06c23583061a6422a73212dc61
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-05-24 12:20:53 +0900

    자바

---
+++ 자바연습폴더/연습.java
@@ -0,0 +1,17 @@
+import java.io.*;;
+
+class park {
+    int field1;
+    String field2;
+    public void method() {
+        System.out.println("hello! world!");
+    }
+
+}
+public class prog {
+	public static void main(String[] args) {
+		park mypark = new park();
+		mypark.method();
+	}
+}
+
