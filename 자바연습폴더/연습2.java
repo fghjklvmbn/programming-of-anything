36608932dcfd1704cbc209f81b815a4ad13aa520
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-05-25 07:10:17 +0900

    연습

---
+++ 자바연습폴더/연습2.java
@@ -0,0 +1,12 @@
+public class 연습2 {
+    public static void main(String[] args) {
+        final int a =30;
+        float num1 = 1.23456789f;
+        double num2 = 1.23456789;
+
+
+        System.out.println(a);
+        System.out.println("float형 변수 num1 : " + num1);
+        System.out.println("double형 변수 num2 : " + num2);
+    }
+}
