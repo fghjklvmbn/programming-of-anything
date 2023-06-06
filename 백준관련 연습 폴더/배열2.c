dbf5b674a907f7c0495c1f099e8275f7b3b1b8d1
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-06-07 03:47:27 +0900

    연습

--- 백준관련 연습 폴더/배열2.c
+++ 백준관련 연습 폴더/배열2.c
@@ -13,13 +13,14 @@ int main() {
     // 숫자 걸러내는 코드(문제있음)
     for (int i = 1; i < a; i++){
         if(array[i] < b){
-            for (int j = 1; j < b; i++){
-                array[i] = array1[j];
+            for (int j = 1; j <= b; j++){
+                array1[j]=array[i];
             }
         }
     }
-    for (int i = 1; i <= b; i++){
-        printf("%d ", array1[b]);
-    }
+    for (int k = 1; k < b; k++)
+        {
+            printf("%d ", array1[k]);
+        }
     return 0;
 }
