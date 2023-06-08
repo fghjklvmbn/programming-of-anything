bf76563f9970164357fdffc17d7a68f4596982c9
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-06-09 03:37:02 +0900

    자바연습

--- 자바연습폴더/연습9.java
+++ 자바연습폴더/연습9.java
@@ -1,33 +1,54 @@
-import java.util.Scanner;
-class park{
-    public static void sub1(String[] args){
-        int a;
-        int b;
-        String c;
-    }
-};
-class park1 extends park{
-    private static void sub2(String[] args){
-        String d;
-        String e;
-    }
-};
-public class 연습9 {
-    public static void main(String[] args) {
+// import java.util.Scanner;
+
+// import javax.management.MBeanOperationInfo;
+// class car{
+//     public static void sub1(String[] args){
+//         String modelname = "람보르기니";
+//         int modelyear = 2023;
+//     }
+// };
+// class extra extends car{
+//     private static void sub2(String[] args){
+//         String color = "R";
+//         int maxSpeed = 250;
+//     }
+// };
+// public class 연습9 {
+//     public static void main(String[] args) {
         
-        // 클래스 정의
-        String a;
-        park p = new park();
-        park1 p1  = new park1();
-        //시스템 인풋 스트림 통해 입력 
-        Scanner sc = new Scanner(System.in);
-        a = sc.nextLine();
+//         // 클래스 정의
+//         String a;
+//         String b;
+//         int age = 18;
+//         car.sub1(args);
+//         car p = new car();
+//         extra p1  = new extra();
+//         //시스템 인풋 스트림 통해 입력 
+//         Scanner sc = new Scanner(System.in);
+//         a = sc.nextLine();
+//         b = p1.toString();
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
 
-        System.out.println(a);
+// 출력 코드 빌드업
+class age {
+    private int Age;
+    public void setAge(int _age){
+        Age = _age;
+    }
+    public String toString() {
+        return "나이는 : " + Age; // 리턴값을 나이는 : (숫자) 이렇게 출력되게끔 작성
+    }
+}
 
-        System.out.println(p instanceof park);
-        System.out.println(p1 instanceof park);
-        System.out.println(p instanceof park1);
-        System.out.println(p1 instanceof park1);
+//상속
+public class 연습9 extends age{
+    public static void main(String[] args){
+        age ag = new age();
+        ag.setAge(31);
+
+        System.out.println(ag.toString());
     }
 }
