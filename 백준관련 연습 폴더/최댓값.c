4300f9aa0c258695cdb402f36849e352d565dde1
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-06-27 14:44:04 +0900

    문제풀이 푸쉬

--- 백준관련 연습 폴더/최댓값.c
+++ 백준관련 연습 폴더/최댓값.c
@@ -5,31 +5,19 @@
 int main(){
 
     int a[n];
-
-    for(int i = 0; i<n; i++){
-        scanf("%d", &a[i]);
-    }
     
-    int max = 0;
-    int tmp = 0;
+    int tmp, max = 0;
+
     // 최댓값 코드
     for(int i = 0;i<n;i++){
-        if(i==0){
+        // 입력부분
+        scanf("%d", &a[i]);
+
+        // 분류과정 설명: max 값이 a[i]보다 크면 해당사항 안되어 패스하고 그 반대의 상황이면 밑의 반복문으로 max값을 갱신하고 i+1값을 tmp로 넣는다.
+        if(max < a[i]){
             max = a[i];
-            tmp += 1;
-        }
-        if(i>=1){
-            if (max < a[i]){
-                max = a[i];
-                tmp += 1;
-            } else if(max == a[i]){
-                max = max;
-                tmp += 1;
-            } else {
-                continue;
-                tmp += 1;
-            }
-        }
+            tmp = i + 1;
+        }   
     }
     printf("%d\n", max);
     printf("%d", tmp);
