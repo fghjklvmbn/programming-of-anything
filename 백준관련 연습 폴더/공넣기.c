0ea3e55bf162ee8bbcfaf842dafcaa919372ac68
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-06-30 23:34:49 +0900

    asdf

--- 백준관련 연습 폴더/공넣기.c
+++ 백준관련 연습 폴더/공넣기.c
@@ -1,11 +1,35 @@
 #include<stdio.h>
 
 int main(){
+    // n,m 변수 추가 및 입력받기
     int n;
-
     scanf("%d", &n);
-    int m[3];
-
-
-    
+    int m;
+    scanf("%d", &m);
+    // 중요한 배열집합 + 초기화
+    int sum[n];
+    for (int i = 1; i <= n; i++){
+        sum[n] = 0;
+    }
+    // 나머지 필요 변수 초기화
+    int j,k,l;
+    // 메인 분류 코드
+    for (int i = 1; i < m+1; i++){
+        scanf("%d %d %d", &j,&k,&l);
+        if (j==k){
+            sum[j] = 0;
+            sum[j] = l;
+        }else if (j<k){
+            for(int j; j<=k+1; j++){
+                sum[j] = 0;
+            }
+            sum[j] = l;
+        } else if(j>k){
+            return 1;
+        }
+    }
+    for (int x = 1; x <= n; x++){
+        printf("%d ", sum[x]);
+    }
+    return 0;
 }
