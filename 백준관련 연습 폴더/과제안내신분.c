c5e7b5f1373f6ee4682658e1e89fec0a8d3f5371
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-08-01 18:24:08 +0900

    Update 과제안내신분.c

--- 백준관련 연습 폴더/과제안내신분.c
+++ 백준관련 연습 폴더/과제안내신분.c
@@ -1,34 +1,39 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main(){
-    int s[30];
-    int s1[30];
-    int tmp = 1;
-    int tmp1 = 0;
-    for (int i = 0; i < 30; i++){
-        s[i] = tmp;
-        tmp++;
+
+int compare(const void *a, const void *b);
+
+int main(void){
+
+    int arr[30];
+    int tmp;
+
+    for (int i = 0; i < 28; i++){ 
+        scanf("%d", &arr[i]);
     }
+    
+    qsort(arr, 29, sizeof(arr[0]), compare);
 
-    for (int i = 0; i < 30; i++){
-        scanf("%d", &s1[i]);
-        if(i>1){
-            if(s1[i-1]<s1[i]){
-            	tmp1 = s1[i];
-	            s1[i] = s1[i-1];
-	            s1[i-1] = tmp1;
-            } else {
-	            tmp1 = s1[i-1];
-	            s1[i-1] = s1[i];
-	            s1[i] = tmp1;
-            }
-        }    
-    }
-
-    for (int i = 0; i < 30; i++)
+    for (int i = 0; i < 28; i++)
     {
-        printf("%d ", s1[i]);
+        printf("%d ", arr[i]);
     }
     
     return 0;
+}
+
+int compare(const void *a, const void *b){
+    if(*(int*)a > *(int*)b){
+        return 1;
+    } else if(*(int*)a < *(int*)b){
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+int result(void){
+
+
 }
