dd5c11ed03f6fcb75b898918ba609149c5d30e46
fghjklvmbn <66707337+fghjklvmbn@users.noreply.github.com>
2023-10-11 15:52:22 +0900

    test

---
+++ c언어_폴더/문서검색.c
@@ -0,0 +1,22 @@
+// 브루트포스 알고리즘(완전탐색) 사용
+#include<stdio.h>
+
+int main(){
+    char document[2500];
+    char search[50];
+    scanf("%c", &document);
+    scanf("%c", &search);
+    int a, b;
+    a = search.length;
+    b = ;
+    while (document!=EOF){
+        document
+        if (search[a] == document[a])
+        {
+            /* code */
+        }
+        
+    }
+
+    return 0;
+}
