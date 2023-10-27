3b0d6cefe677fab658c7eb850c300c28215de486
park <dsapoi881@gmail.com>
2023-10-27 11:33:01 +0900

    asdf

--- 대학교자바/자바8강/예제3.java
+++ 대학교자바/자바8강/예제3.java
@@ -1,5 +1,12 @@
 package 대학교자바.자바8강;
+import 대학교자바.자바6강.예제1;
 
 public class 예제3 {
-    
+    public static void main(String[] args) {
+        SamsungPhone s = new SamsungPhone();
+        s.printLogo();
+        s.sendcall();
+        s.flash();
+        s.receivecall();
+    }
 }
