5308c206486aab49b12cf14dc08d2cbe432cb28c
fghjklvmbn <dsapoi881@gmail.com>
2023-11-10 14:10:32 +0900

    ain

---
+++ 백준관련_연습_폴더/별찍기5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    for (int i = a; i > 0; i--){
+        for (int k = i; k < a; k++){
+            printf(" ");   
+        }
+        for (int j = 1; j <= i; j++){
+            printf("*");
+            if (j==i){
+                printf("\n");
+            }
+            
+        }
+    }   
+    return 0;
+}
