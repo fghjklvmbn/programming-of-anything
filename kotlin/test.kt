c36e7a302f850711548fc593d9d5428424a46105
fghjklvmbn <dsapoi881@gmail.com>
2023-11-20 15:46:34 +0900

    commit more files

---
+++ kotlin/test.kt
@@ -0,0 +1,6 @@
+fun test(a, b){
+    a = 10;
+    b = 20;
+    Int sum = 0;
+    a + b = sum;
+}
---
+++ 백준관련_연습_폴더/수정렬하기3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int coperation(const int *a, const int *b){}
+int main(){
+
+    return 0;
+} 
