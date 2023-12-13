e7731c71415bfdafaf5ec0473d7325b4e356ca7a
fghjklvmbn <dsapoi881@gmail.com>
2023-12-13 09:01:12 +0900

    rework

--- 각종프로젝트연습/프로젝트(시험)/회원가입페이지/test.js
+++ 각종프로젝트연습/프로젝트(시험)/회원가입페이지/test.js
@@ -1,7 +1,9 @@
+// c (ID)값 오류 왜 오류나는지는 몰?루
+
 function welcome(){
+    let c = docuemnt.getElementById("ID").value;
     let a = document.getElementById("PW").value;
     let b = document.getElementById("PW_re").value;
-    let c = docuemnt.getElementById("ID").value;
     let e = document.getElementById("email").value;
     if(a != b){
         alert("비밀번호 재확인 결과 일치하지 않습니다. 다시 입력해주세요.");
