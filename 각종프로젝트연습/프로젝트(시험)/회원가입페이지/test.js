61043b099d1459286463b4109327be8c0262622e
fghjklvmbn <dsapoi881@gmail.com>
2023-12-14 17:44:38 +0900

    asdf

--- 각종프로젝트연습/프로젝트(시험)/회원가입페이지/test.js
+++ 각종프로젝트연습/프로젝트(시험)/회원가입페이지/test.js
@@ -1,4 +1,4 @@
-// c (ID)값 오류 왜 오류나는지는 몰?루
+// c(ID)값 오류 왜 오류나는지는 몰?루
 
 function welcome(){
     let c = docuemnt.getElementById("ID").value;
@@ -6,11 +6,15 @@ function welcome(){
     let b = document.getElementById("PW_re").value;
     let e = document.getElementById("email").value;
     if(a != b){
-        alert("비밀번호 재확인 결과 일치하지 않습니다. 다시 입력해주세요.");
+        alert("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
         return ;
     } else {
         // init
-        window.localStorage.setItem("ID", c);
+        try{
+            window.localStorage.setItem("ID", c);
+        }catch {
+            
+        }
         window.localStorage.setItem("PW", b);
         window.localStorage.setItem("email", e);
         alert("가입을 환영합니다!" );
