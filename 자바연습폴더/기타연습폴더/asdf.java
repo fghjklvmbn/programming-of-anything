bea4dfc4e74026a2d522893b16336d9f2029c6cf
fghjklvmbn <dsapoi881@gmail.com>
2024-01-02 06:30:19 +0900

    sasdddf

---
+++ 자바연습폴더/기타연습폴더/asdf.java
@@ -0,0 +1,65 @@
+
+// ㅁㄴㄹㅇㄴㅁㄹㄴ
+/*
+ * ㄹㅇㅁㄴㄹㄴㅇ
+ * ㄹㅁㄴㅇㄹㅇㄴㅁ
+ * ㄹㅇㄴㅁㄹㅇㄴㅁ
+ */
+
+package 자바연습폴더.기타연습폴더;
+
+//import java.util.*;
+
+public class asdf extends asdfasdf {
+    public static void main(String[] args) {
+        // Scanner s = new Scanner(System.in);
+        // try {
+        //     int a = s.nextInt();
+        //     System.out.println(a);
+        // } catch (Exception e) {
+        //     System.out.println("들어간 숫자가 문자가 포함되어있거나 문자열 입니다. 다시 실행해주세요");
+        // } finally {
+        //     s.close();
+        // }
+
+        asdfiles();
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println('s');
+        System.out.println("parkd");
+
+        int a = 123;
+        char x = 'x';
+        double y = 1.0234;
+        double z = 1.28943;
+
+        double pi = Math.PI;
+
+        System.out.println(a + x + y + z + pi);
+
+        final int a1 = 192;
+
+        System.out.println(a1);
+        
+        // byte : -128 ~ +127(256)
+        byte a12 = 1;
+
+        // short : -32768 ~ +32767
+        short a123 = 2;
+
+        // byte a13 = 128;
+        
+
+
+
+    }
+}
+
+/**
+ * asdf
+ */
+class asdfasdf {
+    static void asdfiles() {
+        System.out.println("asdf");
+    }
+}
