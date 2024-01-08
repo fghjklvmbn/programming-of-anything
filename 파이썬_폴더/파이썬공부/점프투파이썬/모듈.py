2e71df7e25d901c5c5d921fc7a1342ae0e883dfd
fghjklvmbn <dsapoi881@gmail.com>
2024-01-09 00:46:59 +0900

    add file : a lot of python practice files

---
+++ 파이썬_폴더/파이썬공부/점프투파이썬/모듈.py
@@ -0,0 +1,13 @@
+
+# 덧셈 / 뺄셈 모듈
+def sum(a,b):
+    return a+b;
+    
+def sub(a,b):
+    return a-b;
+
+    
+# if __name__ = __main__ : 직접 실행하는것에 대해 실행됨 / import되는 경우는 비활성화
+if __name__ == "__main__" :
+    print("21");
+    print("30");
