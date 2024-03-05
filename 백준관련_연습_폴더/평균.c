1d94077c5a7179270bf3062683a95085b78f3be3
fghjklvmbn <dsapoi881@gmail.com>
2024-03-05 18:00:12 +0900

    qwer

--- 백준관련_연습_폴더/평균.c
+++ 백준관련_연습_폴더/평균.c
@@ -2,11 +2,15 @@
 
 int main(){
     int n;
+    scanf("%d", &n);
+    double sum = 0;
     int result[n+1];
     for (int i = 0; i < n; i++){
         scanf("%d", &result[i]);
+        sum += result[i];
     }
-    
+
+    printf("%0.1f", sum/n);
 
     return 0;
 }
