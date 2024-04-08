d7b6ebe59d1564422f9f548993467de424f70366
fghjklvmbn <dsapoi881@gmail.com>
2024-04-08 15:10:09 +0900

    asdf

--- 백준관련_연습_폴더/a+b10.c
+++ 백준관련_연습_폴더/a+b10.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int a,b;
-    int result;
+    // int a,b;
+    // int result;
 
-    scanf("%d %d", &a, &b);
-    result = a+b;
+    // scanf("%d %d", &a, &b);
+    // result = a+b;
 
-    printf("%d", result);
-    return 0;
+    // printf("%d", result);
+    // return 0;
+
+    
 }
